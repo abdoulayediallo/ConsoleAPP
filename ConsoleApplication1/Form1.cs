011d56d4c17d77af2fccd021af5adfe0673bba0a
Diallo Abdoulaye <d_abdoulaye@outlook.Com>
2016-09-30 17:14:10 +0200

    Forms added without integration with console logic

---
+++ ConsoleApplication1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApplication1
+{
+    public partial class Form1 : Form
+    {
+        string src = "", dst ="";
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = ""; label2.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Choisissez le fichier à convertir en CSV";
+            ofd.Multiselect = false;
+            DialogResult dr = new DialogResult();
+            if(dr == DialogResult.OK)
+            {
+                label1.Text = ofd.FileName;
+                src = ofd.FileName;
+            }
+            
+        }
+    }
+}
